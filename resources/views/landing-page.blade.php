9567a61c3b8853c9ef6e18aed6e97e86a3222bc6
kikinazifah <nazifakhitah@gmail.com>
2026-02-26 14:32:44 +0700

    aduh pusingnya

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -10,7 +10,7 @@
 
                 <!-- JUDUL -->
                 <h1 class="text-white text-2xl md:text-4xl font-bold mb-4 max-w-4xl leading-tight">
-                    Halo Ini Website Green Drop 
+                    Halo Ini Website Green Drop, Platform Donasi Barang untuk Meringankan Beban Sesama
                 </h1>
 
                 <!-- PARAGRAF DI TENGAH NICH -->
